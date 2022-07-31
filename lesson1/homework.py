2a7c536c8bf207774c1b102cf3a40c6b3931142b
raflat <adouby01@gmail.com>
2022-07-31 17:45:04 +0200

    updated vairbale names and comments

--- lesson1/homework.py
+++ lesson1/homework.py
@@ -1,30 +1,32 @@
 """
     Creare uno script di python che simuli il login ad un sito di casinò.
     Si chieda all'utente: nome, cognome ed età.
-    Si verifichi che il nome e il cognome siano più lunghi di un carattere e che l'età sia almeno
-    18 anni.
-    Se tutto è corretto stampare una stringa di conferma, altrimenti comunicare l'errore all'utente.
+    Si verifichi che il nome e il cognome siano più lunghi di un carattere e
+    che l'età sia almeno 18 anni.
+    Se tutto è corretto stampare una stringa di conferma,
+    altrimenti comunicare l'errore all'utente.
     Si consideri che l'utente fornità sempre dati di tipo corretto.
-    Stampare il resoconto dei dati, assicurandosi che nome e cognome inizino con la maiuscola.
+    Stampare il resoconto dei dati, assicurandosi che nome e
+    cognome inizino con la maiuscola.
 """
 
 print("Inserire dati utente casinò")
-nome = input("> nome: ")
-cognome = input("> cognome: ")
-eta = int(input("> età: "))
+name = input("> nome: ")
+surname = input("> cognome: ")
+age = int(input("> età: "))
 
-errore = True
-if len(nome) < 2:
+error = True
+if len(name) < 2:
     print("Errore: il nome deve almeno avere 2 caratteri.")
-elif len(cognome) < 2:
+elif len(surname) < 2:
     print("Errore: il cognome deve almeno avere 2 caratteri.")
-elif eta < 18:
+elif age < 18:
     print("Errore: l'età deve almeno essere 18 anni.")
 else:
     errore = False
 
 if not errore:
     print("\nResoconto dati")
-    print("> nome: ", nome.capitalize())
-    print("> cognome: ", cognome.capitalize())
-    print("> età: ", eta)
+    print("> nome: ", name.capitalize())
+    print("> cognome: ", surname.capitalize())
+    print("> età: ", age)
